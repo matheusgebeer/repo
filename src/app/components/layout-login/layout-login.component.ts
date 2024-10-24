e101de24027f8732a8b86b02130647bf70d188fc
matheusgebeer <matheusgeber@gmail.com>
2024-10-24 09:00:35 -0300

    Add files via upload

---
+++ src/app/components/layout-login/layout-login.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout-login.component.html',
+  styleUrl: './layout-login.component.scss'
+})
+export class LayoutLoginComponent {
+  @Input() title: string ="";
+  @Input() primaryBtnText: string ="";
+  @Input() secondaryBtnText: string ="";
+  @Input() disablePrimaryBtn: boolean = true;
+  @Output("submit") onSubmit = new EventEmitter();
+
+  @Output("navigate") onNavigate = new EventEmitter();
+  
+  submit() {
+    this.onSubmit.emit();
+  }
+
+  navigate() {
+    this.onNavigate.emit();
+  }
+
+}
